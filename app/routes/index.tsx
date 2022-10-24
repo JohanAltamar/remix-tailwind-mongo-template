2902f2339201bc2546c0782d79948426fe01a534
Johan Altamar <johan.altamar.dev@gmail.com>
2022-10-23 19:50:48 -0500

    Initialize remix project

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="h-screen bg-gray-200 flex justify-center items-center">
+      <h2 className="text-blue-600 font-extrabold text-5xl">
+        This is a remix and TailwindCSS project!
+      </h2>
+    </div>
+  );
+}
